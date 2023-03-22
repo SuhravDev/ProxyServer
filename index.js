e1e981b2f700690acc6347b05424b0e18b9b88b2
SuhravHussen <suhravshan@gmail.com>
2023-03-22 17:27:51 +0600

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://proxy-client-sage.vercel.app/"],
+    origin: ["http://localhost:3001", "https://proxy-client-sage.vercel.app"],
     credentials: true,
   })
 );
