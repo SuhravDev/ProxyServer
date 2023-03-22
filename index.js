9a739ff47b883e1bb5b7a349df9d172f448377f6
SuhravHussen <suhravshan@gmail.com>
2023-03-22 13:17:19 +0600

    first commit

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get("/auth", (req, res) => {
   res.send("Hello AUTH!");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Example app listening on port 3000!");
 });
